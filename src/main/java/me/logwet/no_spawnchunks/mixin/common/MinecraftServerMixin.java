18a4c86d861074aecc5f34dd56fd19ed2d34c5c5
logwet <logwet@users.noreply.github.com>
2021-10-20 22:39:16 +1100

    Logging for debugging

--- src/main/java/me/logwet/no_spawnchunks/mixin/common/MinecraftServerMixin.java
+++ src/main/java/me/logwet/no_spawnchunks/mixin/common/MinecraftServerMixin.java
@@ -1,10 +1,12 @@
 package me.logwet.no_spawnchunks.mixin.common;
 
+import me.logwet.no_spawnchunks.NoSpawnchunks;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ChunkTicketType;
 import net.minecraft.server.world.ServerChunkManager;
 import net.minecraft.util.Unit;
 import net.minecraft.util.math.ChunkPos;
+import org.apache.logging.log4j.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
 
@@ -13,16 +15,19 @@ public abstract class MinecraftServerMixin {
 
     @Redirect(method = "prepareStartRegion", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerChunkManager;addTicket(Lnet/minecraft/server/world/ChunkTicketType;Lnet/minecraft/util/math/ChunkPos;ILjava/lang/Object;)V"))
     private void redirectAddTicket(ServerChunkManager serverChunkManager, ChunkTicketType<Unit> ticketType, ChunkPos pos, int radius, Object argument) {
+        NoSpawnchunks.log(Level.INFO, "Prevented spawn chunks loading on initial world load.");
         return;
     }
 
     @Redirect(method = "prepareStartRegion", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerChunkManager;getTotalChunksLoadedCount()I"))
     private int redirectGetTotalChunksLoadedCount(ServerChunkManager serverChunkManager) {
+        NoSpawnchunks.log(Level.INFO, "Redirected chunk count call.");
         return 1;
     }
 
     @ModifyConstant(method = "prepareStartRegion", constant = @Constant(intValue = 441))
     private int modifyNumChunksToWaitFor(int value) {
+        NoSpawnchunks.log(Level.INFO, "Modified targeted chunk count constant.");
         return 1;
     }
 
--- src/main/java/me/logwet/no_spawnchunks/mixin/common/ServerWorldMixin.java
+++ src/main/java/me/logwet/no_spawnchunks/mixin/common/ServerWorldMixin.java
@@ -1,10 +1,12 @@
 package me.logwet.no_spawnchunks.mixin.common;
 
+import me.logwet.no_spawnchunks.NoSpawnchunks;
 import net.minecraft.server.world.ChunkTicketType;
 import net.minecraft.server.world.ServerChunkManager;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Unit;
 import net.minecraft.util.math.ChunkPos;
+import org.apache.logging.log4j.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -14,11 +16,13 @@ public abstract class ServerWorldMixin {
 
     @Redirect(method = "setSpawnPos", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerChunkManager;removeTicket(Lnet/minecraft/server/world/ChunkTicketType;Lnet/minecraft/util/math/ChunkPos;ILjava/lang/Object;)V"))
     private void redirectRemoveTicket(ServerChunkManager serverChunkManager, ChunkTicketType<Unit> ticketType, ChunkPos pos, int radius, Object argument) {
+        NoSpawnchunks.log(Level.INFO, "Prevented spawn chunks unloading on spawn point change.");
         return;
     }
 
     @Redirect(method = "setSpawnPos", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerChunkManager;addTicket(Lnet/minecraft/server/world/ChunkTicketType;Lnet/minecraft/util/math/ChunkPos;ILjava/lang/Object;)V"))
     private void redirectAddTicket(ServerChunkManager serverChunkManager, ChunkTicketType<Unit> ticketType, ChunkPos pos, int radius, Object argument) {
+        NoSpawnchunks.log(Level.INFO, "Prevented spawn chunks loading on spawn point change.");
         return;
     }
 
