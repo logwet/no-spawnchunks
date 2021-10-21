1c5e445aa6ba6bc13c95291ce13ae811784dfdb7
logwet <logwet@users.noreply.github.com>
2021-10-21 16:40:36 +1100

    Rename mixins to be more descriptive + Chunk Mod >1.1.0 support

--- src/main/java/me/logwet/no_spawnchunks/mixin/common/MinecraftServer_RedirectAddStartTicketMixin.java
+++ src/main/java/me/logwet/no_spawnchunks/mixin/common/MinecraftServer_RedirectAddStartTicketMixin.java
@@ -9,28 +9,14 @@ import net.minecraft.util.math.ChunkPos;
 import org.apache.logging.log4j.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(MinecraftServer.class)
-public abstract class MinecraftServerMixin {
+public class MinecraftServer_RedirectAddStartTicketMixin {
 
     @Redirect(method = "prepareStartRegion", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerChunkManager;addTicket(Lnet/minecraft/server/world/ChunkTicketType;Lnet/minecraft/util/math/ChunkPos;ILjava/lang/Object;)V"))
     private void redirectAddTicket(ServerChunkManager serverChunkManager, ChunkTicketType<Unit> ticketType, ChunkPos pos, int radius, Object argument) {
         NoSpawnchunks.log(Level.INFO, "Prevented spawn chunks loading on initial world load.");
     }
-
-    @Redirect(method = "prepareStartRegion", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerChunkManager;getTotalChunksLoadedCount()I"))
-    private int redirectGetTotalChunksLoadedCount(ServerChunkManager serverChunkManager) {
-        NoSpawnchunks.log(Level.INFO, "Redirected chunk count call.");
-        return 1;
-    }
-
-    @ModifyConstant(method = "prepareStartRegion", constant = @Constant(intValue = 441))
-    private int modifyNumChunksToWaitFor(int value) {
-        NoSpawnchunks.log(Level.INFO, "Modified targeted chunk count constant.");
-        return 1;
-    }
-
+    
 }
---
+++ src/main/java/me/logwet/no_spawnchunks/mixin/common/MinecraftServer_SkipWaitForChunkGenMixin.java
@@ -0,0 +1,35 @@
+package me.logwet.no_spawnchunks.mixin.common;
+
+import me.logwet.no_spawnchunks.NoSpawnchunks;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerChunkManager;
+import org.apache.logging.log4j.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = MinecraftServer.class, priority = 1100)
+public abstract class MinecraftServer_SkipWaitForChunkGenMixin {
+
+    @Redirect(method = "prepareStartRegion", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerChunkManager;getTotalChunksLoadedCount()I"))
+    private int redirectGetTotalChunksLoadedCount(ServerChunkManager serverChunkManager) {
+        NoSpawnchunks.log(Level.INFO, "Redirected chunk count call.");
+        return 1;
+    }
+
+    @ModifyConstant(method = "prepareStartRegion", constant = @Constant(intValue = 441))
+    private int modifyNumChunksToWaitFor(int value) {
+        NoSpawnchunks.log(Level.INFO, "Modified targeted chunk count constant.");
+        return 1;
+    }
+
+    /**
+     * @author Gregor0410
+     * @reason https://github.com/Mario0051/chunk-mod/issues/3
+     */
+    @Inject(method = "prepareStartRegion", at = @At("TAIL"))
+    private void onPrepareStartRegion(CallbackInfo info) {
+        ((MinecraftServer) (Object) this).save(false,false,false);
+    }
+
+}
